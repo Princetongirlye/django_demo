5b887d882d795c97e5a1982ac0b06dba591db9c6
Princetongirlye <princeton910207@gmail.com>
2019-12-01 17:36:00 +0800

    book management system

---
+++ book_management_system/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BookManagementSystemConfig(AppConfig):
+    name = 'book_management_system'
---
+++ book_management_system/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+# 图书管理系统 书 作者 出版社
+
+
+class Publisher(models.Model):
+    # 自增的ID主键
+    id = models.AutoField(primary_key=True)
+    # 创建唯一不为空的字段
+    name = models.CharField(max_length=64, null=False, unique=True)
+
